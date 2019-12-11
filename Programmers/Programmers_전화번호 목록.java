3f52e116e2da32200e68d2955b51b01db225d2f0
최푸름 <poo1994@hanmail.net>
2019-12-12 00:32:52 +0900

    Level 2 #Hashmap

---
+++ Programmers/Programmers_전화번호 목록.java
@@ -0,0 +1,15 @@
+package Traning;
+import java.util.*;
+
+
+class Solution {
+    public boolean solution(String[] phoneBook) {
+       for(int i=0; i<phoneBook.length-1; i++) {
+            for(int j=i+1; j<phoneBook.length; j++) {
+                if(phoneBook[i].startsWith(phoneBook[j])) {return false;}
+                if(phoneBook[j].startsWith(phoneBook[i])) {return false;}
+            }
+        }
+        return true;
+    }
+}
