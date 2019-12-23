0509839e45700775d7bdc6cd86e77aa6d832f7c1
최푸름 <poo1994@hanmail.net>
2019-12-24 00:20:19 +0900

    #정열적인 정렬

---
+++ BOJ/BOJ_16212.java
@@ -0,0 +1,27 @@
+package Traning;
+
+//StringTokenizer st = new StringTokenizer(br.readLine());
+
+import java.util.*;
+import javax.swing.text.AbstractDocument.BranchElement;
+import java.io.*;
+
+public class Main {
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int n=Integer.parseInt(br.readLine());
+		int [] arr= new int[n];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=0;i<n;i++) {
+			arr[i]=Integer.parseInt(st.nextToken());
+		}//arr입력 
+		Arrays.sort(arr);
+		for(int i=0;i<n;i++) {
+			System.out.print(arr[i]+" ");
+		}
+		
+		
+		}
+
+}
