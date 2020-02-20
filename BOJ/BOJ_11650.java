734a5258f2546fb8bb4d38d8ed381bc5f1e67fb6
Choipureum <55127127+choipureum@users.noreply.github.com>
2020-02-20 18:12:53 +0900

    #좌표 정렬하기

---
+++ BOJ/BOJ_11650.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+   public static void main(String[] args) throws Exception {
+       BufferedReader br=new BufferedReader(new InputStreamReader(System.in));      
+       int n=Integer.parseInt(br.readLine()); //testcase 개수
+       int [][] arr =new int[n][2]; //다중배열 생성
+    
+       for(int i=0;i<n;i++) {
+    	   StringTokenizer st=new StringTokenizer(br.readLine());
+    	   arr[i][0]=Integer.parseInt(st.nextToken()); //처음넣기
+    	   arr[i][1]=Integer.parseInt(st.nextToken()); //다음넣기   	
+       }
+       Arrays.sort(arr,new Comparator <int[]>() 
+       {
+		@Override
+		public int compare(int[] o1, int[] o2) {
+			if(o1[0]==o2[0]) {
+				return Integer.compare(o1[1], o2[1]);
+			}
+				return Integer.compare(o1[0],o2[0]);			
+		}    	   
+       });
+       	
+         for(int i=0;i<n;i++) {
+        	 System.out.println(arr[i][0]+" "+arr[i][1]);
+         }
+   } //method end
+} //class end
+
