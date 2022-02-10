1e3116e4d02b21d1207e16eb44077078253d2a4e
PuReum Choi <poo1994@hanmail.net>
2022-02-10 15:23:36 +0900

    Update 12Longest Substring With At Most Two Distinct.java

--- CodingTest/인프런/01StringArray/12Longest Substring With At Most Two Distinct.java
+++ CodingTest/인프런/01StringArray/12Longest Substring With At Most Two Distinct.java
@@ -1,46 +1,31 @@
 import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-class Main {
-  //Longest Substring With At Most Two Distinct
-  public static void main(String[] args) 
-  {
-    String s = "ccaabbb";
-    System.out.println(solve(s));
-  }
-  public static int solve(String s)
-  {
-    //1 ds
-    int start =0,end=0,length=0, counter=0;
-    Map<Character,Integer> map = new HashMap<>();
-    
-    //2 
-    while(end < s.length())
-    {
-      char endChar = s.charAt(end); //c
-      map.put(endChar, map.getOrDefault(endChar,0)+1);//c=2, a=2, b=3
-      if(map.get(endChar)==1)
-      {
-        counter++;
-      }
-      end++;
+public class Solution {
+    public static void main(String[] args) {
+        String s = "pweeklkogb";
 
-      //3 C 2개삭제
-      while(counter>2)
-      {
-        char startChar = s.charAt(start);
-        map.put(startChar,map.get(startChar)-1);
-        if(map.get(startChar)==0)
-        {
-          counter--;
-        }
-        start++;
-      }
-
-      length = Math.max(length, end-start);
+        System.out.println(solution(s));
+        //System.out.println(Arrays.toString(solution(matrix)));
+        //System.out.println(Arrays.deepToString(solution(str)));
     }
-    return length;
-  }
- 
-}
+    public static int solution(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        int l=0, r=0, couter=0,max=0;
 
+        while(r<s.length()){
+            char c = s.charAt(r);
+            if(map.containsKey(c)){
+                l = Math.max(l, map.get(c)+1);
+            }
+            map.put(c, r);
+            r++;
+            couter = r-l;
+            max = Math.max(max, couter);
+        }
+        return max;
+    }
+
+}
 
