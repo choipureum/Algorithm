e3c4dfaa4f9db5a7df740eec5c1cd29c52fdf64d
PuReum Choi <poo1994@hanmail.net>
2022-02-10 14:00:52 +0900

    Update 06MeetingRoom.java

--- CodingTest/인프런/01StringArray/06MeetingRoom.java
+++ CodingTest/인프런/01StringArray/06MeetingRoom.java
@@ -1,54 +1,33 @@
 import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-public class MeetingRoom2 {
-	
-	//우선순위 큐(이진트리)
-	public static void main(String[] args) {
-		MeetingRoom2  m2 = new MeetingRoom2();
-		ExInterval in1 = new ExInterval(0,30);
-		ExInterval in2 = new ExInterval(4,9);
-		ExInterval in3 = new ExInterval(10,16);
-		ExInterval in4 = new ExInterval(5,15);
-		
-		ExInterval [] intervals = {in1,in2,in3,in4};
-		System.out.println(m2.solve(intervals));
-		
-	}
-	public int solve(ExInterval [] intervals) {
-		if(intervals ==null || intervals.length ==0) return 0;
-		//1 sorting(lambda)
-		Arrays.sort(intervals, (a,b)-> a.start - b.start);
-		
-		//2 
-		Queue<ExInterval> pq = new PriorityQueue<ExInterval>((ExInterval a, ExInterval b)->a.end-b.end);
-		
-		pq.offer(intervals[0]);
-		
-		//3
-		for(int i=1;i<intervals.length;i++) {
-			if(pq.peek().end <= intervals[i].start) { //회의실 필요 없음
-				pq.poll();
-			}
-			
-			pq.offer(intervals[i]); // 노드 넣기
-		}
-		
-		
-		return pq.size();	
-	}
-	
+public class Solution {
+    public static void main(String[] args) {
+        int [] nums = {2,8,11,14};
+        int [][] integer = {{5,10},{16,20},{0,30}};
+        int target = 16;
+
+        System.out.println(solution(integer));
+    }
+    public static int solution(int[][] integer){
+        Arrays.sort(integer, new Comparator<int[]>(){
+            @Override
+            public int compare(int[] a, int[] b){
+                return a[0] - b[0];
+            }
+        });
+        Queue <int[]> q = new PriorityQueue<int[]>((a,b)->a[1]-b[1]);
+        q.offer(integer[0]);
+        for(int i=1;i<integer.length;i++){
+            if(q.peek()[1]<integer[i][0]){
+                q.poll();     
+            }
+            q.offer(integer[i]);
+        }
+
+        return q.size();
+    }
+        
 }
 
-class ExInterval{
-	int start;
-	int end;
-	
-	ExInterval(){
-		start=0;
-		end=0;
-	}
-	ExInterval(int s,int e){
-		start=s;
-		end=e;
-	}
-}
