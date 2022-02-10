c5317069c698938e6e2bbffeba88a4602f5e355e
PuReum Choi <poo1994@hanmail.net>
2022-02-10 14:16:57 +0900

    Update 04DailyTemperature.java

--- CodingTest/인프런/01StringArray/04DailyTemperature.java
+++ CodingTest/인프런/01StringArray/04DailyTemperature.java
@@ -1,40 +1,28 @@
 import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-/**
- * 
- * @author User
- * 
- */
-public class DailyTemperature {
+public class Solution {
+    public static void main(String[] args) {
+        int [] nums = {73,74,75,71,69,72,76,73};
+        System.out.println(Arrays.toString(solution(nums)));
+    }
+    public static int[] solution(int[] nums){
+        //ds
+        int[] result = new int[nums.length];
+        Stack<Integer> stack = new Stack<>();
 
-	public static void main(String[] args) {
-		DailyTemperature a = new DailyTemperature();
-		int [] nums = {73,74,75,71,69,72,76,73};
-		int [] res = a.solve(nums);
-		
-		System.out.println("=== result ===");
-		for(int i: res) {
-			System.out.print(i+ " ");
-		}
-		
-	}
-	public int [] solve(int [] nums) {
-		//1. ds
-		Stack<Integer> stack = new Stack<Integer>();
-		int [] result= new int[nums.length];
-			
-		//2.for, while algo
-		for(int i=0;i<nums.length;i++) {
-			System.out.println("nums "+i);
-			while(!stack.isEmpty()&& nums[stack.peek()] < nums[i]) { 
-				//73<74
-				int index = stack.pop(); //peek() + remove()
-				System.out.println("i"+i+" index"+index);
-				result[index] = i-index;
-			}			
-			stack.push(i);
-		}		
-		return result;
-	}
-	
+        //for
+        for(int i=0;i<nums.length;i++){
+            //while
+            while(!stack.isEmpty() && nums[stack.peek()] < nums[i]){
+                int index = stack.pop();
+                result[index] = i-index;
+            }
+            stack.push(i);
+        }
+        return result;
+    }
+        
 }
+
